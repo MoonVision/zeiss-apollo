534479f1eb38f8bf51099f1241d3e65d4503a4e9
Lukas SSanner <lukas.sanner@moonvision.io>
2019-02-09 14:04:13 +0100

    geometry ellipses from contour and my scratch

---
+++ vision/geometry.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+def ellipses_of_contour(contour):
+    return cv2.fitEllipse(contour)
+
+
+def attributes_of_ellipses(ellipses):
+    center = ellipses[0]
+    axis = ellipses[1]
+    angle = ellipses[2]
+    area = np.pi * axis[0] * axis[1]
+    return center, axis, angle, area
