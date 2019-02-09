7dfb5facde18881a57caa907206c3199d2ecc39f
Lukas SSanner <lukas.sanner@moonvision.io>
2019-02-09 21:32:01 +0100

    is border checker

--- vision/geometry.py
+++ vision/geometry.py
@@ -12,3 +12,26 @@ def attributes_of_ellipses(ellipses):
     angle = ellipses[2]
     area = np.pi * axis[0] * axis[1]
     return center, axis, angle, area
+
+
+def is_bordertoucher(img, ellipse):
+    dimensions = img.shape
+    if len(dimensions) == 3:
+        h, w, c = dimensions
+    else:
+        h, w = dimensions
+
+    center, axis, angle, _ = attributes_of_ellipses(ellipse)
+
+    cx, cy = center
+    r = min(axis)
+    R = max(axis)
+    if cx + r >= w or cy + r >= h or cx - r <= 0 or cy-r <= 0:
+        return True
+
+    elif cx + R >= w or cy + R >= h or cx - R <= 0 or cy - R <= 0:
+        return True
+    
+    else:
+        return False
+
